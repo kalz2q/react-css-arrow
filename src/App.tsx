b3c495731772436b76420bbd57a610ded4aa9076
kalz2q <katsu95i@gmail.com>
2022-09-08 18:41:13 +0900

    m

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    document.title = "react-css-arrow"
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <p className="arrow sample1-1"></p>
+      <p className="arrow sample1-2"></p>
+      <p className="arrow sample1-3"></p>
+    </div >
   );
 }
 
