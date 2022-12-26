0236e27b491d849c96ac14764b7e38319e2b397e
kalz2q <katsu95i@gmail.com>
2022-12-27 07:17:12 +0900

    M

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="BOX profile icon"></div>
+      <div className="BOX">
+        <div className="centering"></div>
+      </div>
       <div className="BOX">
         <div className="profile icon"></div>
       </div>
+      <div className="BOX profile icon"></div>
+
       <div className="BOX profileIcon"></div>
       <div className="BOX box">
         <div className="leaf"></div>
       </div>
       <div className="BOX belgium"></div>
+      <div className="BOX">  <div className="belgium"></div></div>
+
       <div className="BOX">
         <div className="yellow"></div>
       </div>
