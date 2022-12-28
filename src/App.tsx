e7f0025b83edf09fbeed7abdf0d2a06da0f910c1
kalz2q <katsu95i@gmail.com>
2022-12-28 13:33:28 +0900

    M

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-// temp-profile-icon
-// 元 https://cssicon.space/#/icon/profile
-// idea BOX を切手を集めるように云々
+// apple's logo in css
+// 元 https://codepen.io/kalz2q/pen/OJwMzWM
+
 
 import React from 'react';
 import './App.css';
@@ -8,29 +8,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
-      <div className="BOX">
-        <div className="cloud icon"></div>
+      <div className="apple">
+        <div className="top"><span></span></div>
+        <div className="middle"><span></span></div>
+        <div className="footer"></div>
       </div>
-      <div className="BOX">
-        <div className="centering"></div>
-      </div>
-      <div className="BOX">
-        <div className="profile icon"></div>
-      </div>
-      <div className="BOX profile icon"></div>
-
-      <div className="BOX profileIcon"></div>
-      <div className="BOX box">
-        <div className="leaf"></div>
-      </div>
-      <div className="BOX belgium"></div>
-      <div className="BOX">  <div className="belgium"></div></div>
-
-      <div className="BOX">
-        <div className="yellow"></div>
-      </div>
-      <div className="BOX pink"></div>
     </div >
   );
 }
